75999ad2b27d8cec012f9e5d6f6760291ad9e989
LazyDev <devbutlazy@gmail.com>
2024-07-12 18:46:53 +0300

    Readme

--- source/config.py
+++ source/config.py
@@ -2,9 +2,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DB_NAME: str
-    TELEGRAM_CHAT_ID: str
-    BOT_TOKEN: str
+    DB_NAME: str # Database name
+    TELEGRAM_CHAT_ID: str # Your and bot chat ID in telegram
+    BOT_TOKEN: str # Telegram bot token
 
     model_config = SettingsConfigDict(env_file=".config.env")
 
