abdf299b8fd997de5486a42adf32b4e1063236a9
LazyDev <devbutlazy@gmail.com>
2024-08-18 18:44:57 +0300

    [+] Logger instead of boring prints; [+] Argparse for --create and --upgrade (-c and -u) when starting the app. ( --create/-c - create a migration before starting processes and switch to it
    --upgrade/-u - upgrade to the HEAD (last) migration before starting processes
    )

--- src/app/main/web_api.py
+++ src/app/main/web_api.py
@@ -1,4 +1,6 @@
 import asyncio
+import argparse
+import subprocess
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -10,13 +12,48 @@ from aiogram.enums import ParseMode
 from uvicorn.config import Config
 from uvicorn.server import Server
 
+from ..presentation.logger.logger import logger
 from ..presentation.telegram.handlers.post_handlers import router as post_router
 from ..presentation.telegram.handlers.common_handlers import router as common_router
 from .config.config import settings
 
 
+def init_routers(app: FastAPI) -> None:
+    """
+    Include routers from the presentation layerCommandStart
+    """
+    ...
+
+
+def handle_arguments(args: argparse.Namespace) -> None:
+    """
+    Manage Alembic migrations and start the processes.
+
+    :param args: The parsed arguments from the command line.
+    :return: None
+    """
+    if args.create:
+        logger.info("[~] Creating migration")
+        subprocess.run(["alembic", "revision", "--autogenerate"])
+        logger.info("[+] Migration created, switching to it")
+        subprocess.run(["alembic", "upgrade", "head"])
+        logger.info("[+] Migration switched")
+
+    elif args.upgrade:
+        logger.info("[~] Upgrading to latest migration")
+        subprocess.run(["alembic", "upgrade", "head"])
+        logger.info("[+] Migration up-to-date")
+
+    asyncio.run(start_processes())
+
+
 async def start_telegram_bot() -> None:
-    print("[+] Telegram bot started")
+    """
+    Start the Telegram bot.
+
+    :return: None
+    """
+    logger.info("[+] Telegram bot started\n")
 
     bot = Bot(
         token=settings.BOT_TOKEN,
@@ -28,14 +65,12 @@ async def start_telegram_bot() -> None:
     await dp.start_polling(bot)
 
 
-def init_routers(app: FastAPI) -> None:
-    """
-    Include routers from the presentation layerCommandStart
-    """
-    ...
-
-
 async def start_processes() -> None:
+    """
+    Start all the processes.
+
+    :return: None
+    """
     app = FastAPI(docs_url=None, redoc_url=None)
     app.add_middleware(
         CORSMiddleware,
@@ -52,4 +87,21 @@ async def start_processes() -> None:
 
 
 if __name__ == "__main__":
-    asyncio.run(start_processes())
+    parser = argparse.ArgumentParser(
+        description="Manage Alembic migrations and start the processes."
+    )
+
+    parser.add_argument(
+        "-c",
+        "--create",
+        action="store_true",
+        help="Create a new Alembic revision and switch to it",
+    )
+    parser.add_argument(
+        "-u",
+        "--upgrade",
+        action="store_true",
+        help="Upgrade the database to the latest Alembic revision",
+    )
+
+    handle_arguments(parser.parse_args())
